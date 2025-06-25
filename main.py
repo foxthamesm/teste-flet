af0f0eb801f3ae9c5c2fc0bb8d063b4058170d37
foxthamesm <foxthamesm@gmail.com>
2025-06-25 00:05:00 -0300

    Produtos Front-end Ending

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def main(page: ft.Page):
     page.theme = ft.Theme()
     page.theme_mode = ft.ThemeMode.DARK
 
+
+
+
+
     # cadastrar produto
     list_img_to_cadastrar = []
     file_picker = ft.FilePicker()
     page.overlay.append(file_picker)
 
-    img_container = ft.Row(wrap=True, spacing=5)
+    img_container = ft.Row(wrap=True, spacing=10, )
     frame_images_cadastro = ft.Container(
         content=img_container,
         width=200,
@@ -42,12 +46,13 @@ def main(page: ft.Page):
             file_picker.pick_files(allow_multiple=True, allowed_extensions=["png","jpg", "jpeg"])
 
     btn_add_img_to_cadastrar = ft.Container(
-        content=ft.Icon(name=ft.icons.ADD, size=40),
+        content=ft.Icon(name=ft.Icons.ADD, size=40),
         width=150,
         height=150,
-        bgcolor=ft.colors.GREY_200,
+        bgcolor=ft.Colors.GREY_200,
         alignment=ft.alignment.center,
         border_radius=10,
+        margin=10,
         on_click=add_images_to_cadastrar
     )
 
@@ -95,11 +100,115 @@ def main(page: ft.Page):
             ]
         ),
         visible=False,
-        bgcolor='#838383'
+        bgcolor='#838383',
+        border_radius=30,
+        width=500
     )
 
     def show_cadastro(e):
         frame_cadastro_produto.visible = not frame_cadastro_produto.visible
+        page.update()
+
+    #EDITAR PRODUTO  < ------------
+    list_img_to_editar = []
+
+    img_container_edit = ft.Row(wrap=True, spacing=10, )
+    frame_images_edit = ft.Container(
+        content=img_container_edit,
+        width=200,
+        visible=True
+    )
+
+    def on_files_selected(e: ft.FilePickerResultEvent):
+        if e.files:
+            for f in e.files:
+                if len(list_img_to_editar) < 4:
+                    file_bytes = f.read_bytes()
+                    base64_str = base64.b64encode(file_bytes).decode("utf-8")
+                    list_img_to_editar.append(base64_str)
+                    img_container.controls.append(ft.Image(src_base64=base64_str, width=100, height=100, fit=ft.ImageFit.COVER))
+                    page.update()
+    
+    file_picker.on_result = on_files_selected
+
+    def add_images_to_editar(e):
+        if len(list_img_to_editar) >= 4:
+            page.snack_bar = ft.SnackBar(ft.Text('limite de 4 images atingido !'))
+            page.snack_bar.open = True
+            page.update()
+        else:
+            file_picker.pick_files(allow_multiple=True, allowed_extensions=["png","jpg", "jpeg"])
+
+    btn_add_img_to_editar = ft.Container(
+        content=ft.Icon(name=ft.Icons.ADD, size=40),
+        width=150,
+        height=150,
+        bgcolor=ft.Colors.GREY_200,
+        alignment=ft.alignment.center,
+        border_radius=10,
+        margin=10,
+        on_click=add_images_to_editar
+    )
+
+
+    nome_produto_editar = ft.TextField(label='Nome do Produto', color='#ffffff')
+    Valor_promocional_editar = ft.TextField(label='Valor Promocional', color='#ffffff')
+    codigo_produto_editar = ft.TextField(label='Codigo Produto', color='#ffffff')
+    
+    def mudando_valor_no_dropdown_editar(e):
+        frequencia_exibicao_edit.value = frequencia_exibicao_edit.value
+        page.update()
+
+    frequencia_exibicao_edit = ft.Dropdown(
+        label='Frequência de exibição',
+        hint_text='Selecione...',
+        options=[
+            ft.dropdown.Option("Aleatória"),
+            ft.dropdown.Option("Agendada"),
+            ft.dropdown.Option("Não exibir"),
+            ft.dropdown.Option("Obrigatório"),
+        ],
+        on_change=mudando_valor_no_dropdown_editar
+    )
+
+    controle_visualizacao_edit = ft.Checkbox(label='Controle de Visualização')
+
+    frame_editar_produto = ft.Container(
+        content=ft.Column(
+            controls=[
+                ft.Row(
+                    controls=[
+                        btn_add_img_to_editar,
+                        ft.Column(
+                            controls=[
+                                frame_images_edit,
+                                nome_produto_editar,
+                                Valor_promocional_editar,
+                                codigo_produto_editar,
+                                frequencia_exibicao_edit,
+                                controle_visualizacao_edit
+                            ]
+                        )
+                    ]
+                )
+            ]
+        ),
+        visible=False,
+        bgcolor='#838383',
+        border_radius=30,
+        width=500
+    )
+    
+    #função passada dentro do gerar lista produtos
+    #NÃO TIRAR A DOCTRING ATÉ TER O DICT/JSON COMO PARAMETRO, POIS VAI GERAR UM ERRO
+    def show_editar(e): # data <- data é igual a um dict/json com informação dos atos cadastrados <- add este parametro
+        '''frame_images_edit.value = ''
+        nome_produto_editar.value = ''
+        Valor_promocional_editar.value = ''
+        codigo_produto_editar.value = ''
+        frequencia_exibicao_edit.value = ''
+        controle_visualizacao_edit.value = '''''
+        frame_editar_produto.visible = not frame_editar_produto.visible
         page.update()
 
     # BUSCAR PRODUTO < ------------
@@ -120,30 +229,33 @@ def main(page: ft.Page):
         return ft.Container(
             content=ft.Row(
                 controls=[
-                    carrosel,
-                    ft.Column(
+                    ft.Row(
                         controls=[
-                            ft.Text('Tênis Nike'),
-                            ft.Text('Código Promocional'),
-                            ft.Text('Valor Promocional'),
+                            carrosel,
+                            ft.Column(
+                                controls=[
+                                    ft.Text('Tênis Nike'),
+                                    ft.Text('Código Promocional'),
+                                    ft.Text('Valor Promocional'),
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                spacing=3
+                            )
                         ],
-                        alignment=ft.MainAxisAlignment.CENTER,
-                        spacing=3
+                        alignment=ft.MainAxisAlignment.START,
                     ),
-                    ft.ElevatedButton('open')
-                ],
-                alignment=ft.MainAxisAlignment.START,
-            ),
-            bgcolor='#838383',
-            margin=10,
-            padding=0,
-            width=500,
-            border_radius=30
-        )
+                    ft.ElevatedButton('open', on_click=show_editar)
+                    ],
+                    spacing=80
+                ),
+                    bgcolor='#838383',
+                    margin=10,
+                    padding=0,
+                    width=500,
+                    border_radius=30)
 
     frame_lista_produtos = ft.Column(
         scroll=ft.ScrollMode.AUTO,
-        expand=True
     )
 
     def buscar_produto(e):
@@ -157,7 +269,7 @@ def main(page: ft.Page):
         content=ft.Row(
             controls=[
                 search_produtos,
-                ft.ElevatedButton(icon=ft.icons.SEARCH, text='.', color=ft.colors.WHITE, on_click=buscar_produto)
+                ft.ElevatedButton(icon=ft.Icons.SEARCH, text='.', color=ft.Colors.WHITE, on_click=buscar_produto)
             ]
         ),
         margin=10
@@ -173,8 +285,19 @@ def main(page: ft.Page):
                         ft.ElevatedButton(text='Add Produto', on_click=show_cadastro)
                     ]
                 ),
-                frame_lista_produtos,
-                frame_cadastro_produto
+                ft.Row(
+                    controls=[
+                        frame_lista_produtos,
+                        ft.Column(
+                            controls=[
+                                frame_cadastro_produto,
+                                frame_editar_produto
+                            ]
+                        )
+                    ],
+                    alignment=ft.MainAxisAlignment.START
+
+                ),
             ]
         ),
         visible=False,
@@ -192,11 +315,11 @@ def main(page: ft.Page):
     lista_buttons = ft.Container(
         content=ft.Row(
             controls=[
-                ft.ElevatedButton(text='PRODUTOS', on_click=show_main_produtos, color=ft.colors.WHITE, bgcolor='#222256', width=100, height=37),
-                ft.ElevatedButton(text='USUÁRIOS', color=ft.colors.WHITE, bgcolor='#222256', width=100, height=37),
-                ft.ElevatedButton(text='CAMPANHAS', color=ft.colors.WHITE, bgcolor='#222256', width=110, height=37),
-                ft.ElevatedButton(text='AGENDA', color=ft.colors.WHITE, bgcolor='#222256', width=100, height=37),
-                ft.ElevatedButton(text='RELATÓRIOS', color=ft.colors.WHITE, bgcolor='#222256', width=100, height=37),
+                ft.ElevatedButton(text='PRODUTOS', on_click=show_main_produtos, color=ft.Colors.WHITE, bgcolor='#222256', width=100, height=37),
+                ft.ElevatedButton(text='USUÁRIOS', color=ft.Colors.WHITE, bgcolor='#222256', width=100, height=37),
+                ft.ElevatedButton(text='CAMPANHAS', color=ft.Colors.WHITE, bgcolor='#222256', width=110, height=37),
+                ft.ElevatedButton(text='AGENDA', color=ft.Colors.WHITE, bgcolor='#222256', width=100, height=37),
+                ft.ElevatedButton(text='RELATÓRIOS', color=ft.Colors.WHITE, bgcolor='#222256', width=100, height=37),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             spacing=50
@@ -212,6 +335,8 @@ def main(page: ft.Page):
     )
 
     page.add(layout)
+
+
 
 port = int(os.environ.get("PORT", 8080))
 ft.app(target=main, view=ft.WEB_BROWSER, port=port)
